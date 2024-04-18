f760cb3b501aeb254ca14a187f194dc309e15f36
Kateřina Šebova <katerina.sebova@riganti.cz>
2024-04-18 20:45:38 +0200

    Add base web app

---
+++ Task2/Task2/WebApp/Components/Pages/History.razor.cs
@@ -0,0 +1,25 @@
+﻿using Reports.Core;
+
+namespace WebApp.Components.Pages;
+
+public partial class History
+{
+    private HttpClient _client = new HttpClient() { BaseAddress = new Uri("https://localhost:7108") };
+    public List<string> history = ["January 2024", "December 2023", "November 2023"];
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            var response = await _client.GetAsync("reports/history/");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+           // history = responseBody;// TODO needs parsing but no data to try
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("\nException Caught!");
+            Console.WriteLine("Message :{0} ", e.Message);
+        }
+    }
+}
