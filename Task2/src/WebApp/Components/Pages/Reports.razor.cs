23e9f1aa89bb0113aa2f0f93874047f80f3e7c6e
Tomáš Mikeš <tomas.mikes@riganti.cz>
2024-04-20 15:38:50 +0200

    Add button to manually generate report

--- Task2/src/WebApp/Components/Pages/Reports.razor.cs
+++ Task2/src/WebApp/Components/Pages/Reports.razor.cs
@@ -7,21 +7,51 @@ public partial class Reports
     private Api.Report report;
 
     private bool isLoading = true;
-    private bool isEmpty = false;
+    private bool isGeneratingReport;
+    private bool currentReportExists;
+
+    private string? error;
     
     protected override async Task OnInitializedAsync()
     {
         try
         {
-            report = await reportsClient.CurrentAsync();
+            await GetCurrentMonthReport();
         }
         catch (Exception ex)
         {
-            isEmpty = true;
+            currentReportExists = false;
         }
         finally
         {
             isLoading = false;
         }
+    }
+
+    private async Task GenerateReport()
+    {
+        try
+        {
+            isGeneratingReport = true;
+            await reportsClient.ReportsAsync();
+            await GetCurrentMonthReport();
+        }
+        catch (Exception ex)
+        {
+            error = "Report for this month already exists.";
+        }
+        finally
+        {
+            isLoading = false;
+            isGeneratingReport = false;
+        }
+    }
+
+    private async Task GetCurrentMonthReport()
+    {
+        isLoading = true;
+        report = await reportsClient.CurrentAsync();
+        currentReportExists = true;
+        error = null;
     }
 }
