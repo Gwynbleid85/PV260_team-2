c0f03db59112d9ed0af23152392dc0cc68d6bc69
filiptichy <filip.tichy@oracle.com>
2024-05-12 10:04:48 +0200

    feat: delete user endpoint done

---
+++ Task2/src/ArkFunds.Users/Application/Commands/DeleteUserCommandHandler.cs
@@ -0,0 +1,24 @@
+using ArkFunds.Users.Core;
+using ArkFunds.Users.Core.Events;
+using CommunityToolkit.Diagnostics;
+using Mapster;
+using Marten;
+
+namespace ArkFunds.Users.Application.Commands;
+
+public class DeleteUserCommandHandler
+{
+    public static async Task<User> LoadAsync(DeleteUserCommand command, IQuerySession session)
+    {
+        var user = await session.LoadAsync<User>(command.UserId);
+        Guard.IsNotNull(user);
+        return user;
+    }
+    
+    public static async Task<UserDeleted> Handle(DeleteUserCommand command, User user, IDocumentSession session, CancellationToken cancellationToken)
+    {
+        session.Delete(user);
+        await session.SaveChangesAsync(cancellationToken);
+        return command.Adapt<UserDeleted>();
+    }
+}
