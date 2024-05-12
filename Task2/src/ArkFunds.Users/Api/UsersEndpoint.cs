a174a52b0de95fa251d839b3bdd6723f9f86c111
filiptichy <filip.tichy@oracle.com>
2024-05-12 10:09:22 +0200

    refactor: added documentation to endpoints

--- Task2/src/ArkFunds.Users/Api/UsersEndpoint.cs
+++ Task2/src/ArkFunds.Users/Api/UsersEndpoint.cs
@@ -11,12 +11,24 @@ namespace ArkFunds.Users.Api;
 
 public class UsersEndpoint
 {
+    //TODO: Add authorization
+    /// <summary>
+    /// Get user by ID
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns>user</returns>
     [WolverineGet("/users/{id}")]
     public static User UserGet([Document]User user)
     {
         return user;
     }
     
+    //TODO: remove before production
+    /// <summary>
+    /// Create test user
+    /// </summary>
+    /// <param name="session"></param>
+    /// <returns>user</returns>
     [WolverinePost("/users")]
     public static async Task<User> UserCreate(IDocumentSession session)
     {
@@ -26,6 +38,13 @@ public class UsersEndpoint
         return user;
     }
     
+    //TODO: Add authorization
+    /// <summary>
+    /// Delete user by ID
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="bus"></param>
+    /// <returns>id</returns>
     [WolverineDelete("/users/{id}")]
     public static async Task<UserDeleted> UserDelete(Guid id, IMessageBus bus)
     {
@@ -33,6 +52,14 @@ public class UsersEndpoint
         return await bus.InvokeAsync<UserDeleted>(command);
     }
     
+    //TODO: Add authorization
+    /// <summary>
+    /// Change user email
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="userEmail"></param>
+    /// <param name="bus"></param>
+    /// <returns>id, email</returns>
     [WolverinePut("/users/{id}/email")]
     public static async Task<UserEmailChanged> UserChangeEmail(Guid id, [FromBody] string userEmail, IMessageBus bus)
     {
@@ -40,6 +67,13 @@ public class UsersEndpoint
         return await bus.InvokeAsync<UserEmailChanged>(command);
     }
     
+    //TODO: Add authorization
+    /// <summary>
+    /// Subscribe user
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="bus"></param>
+    /// <returns>id</returns>
     [WolverinePost("/users/{id}/subscription")]
     public static async Task<UserSubscribed> UserSubscribe(Guid id, IMessageBus bus)
     {
