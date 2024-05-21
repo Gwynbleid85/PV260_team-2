ad0fcdb5ce07ba6d73e94b79f406229010acddcc
Milos Hegr <milos.hegr@lifeliqe.com>
2024-05-21 18:56:51 +0200

    fix: GetThreeMonthOldReportQueryHandler accepts now correct query type

--- Task2/src/ArkFunds.Reports/Application/Queries/GetThreeMonthOldReportQueryHandler.cs
+++ Task2/src/ArkFunds.Reports/Application/Queries/GetThreeMonthOldReportQueryHandler.cs
@@ -4,7 +4,8 @@ namespace ArkFunds.Reports.Application.Queries;
 
 public class GetThreeMonthOldReportQueryHandler
 {
-    public static async Task<GetCurrentReportQuery.Response> Handle(GetCurrentReportQuery query, IQuerySession session)
+    public static async Task<GetCurrentReportQuery.Response> Handle(GetThreeMonthOldReportQuery query,
+        IQuerySession session)
     {
         var report = await session.QueryAsync(new CompiledQueries.GetCurrentReportQuery(query.CurrentTime));
 
